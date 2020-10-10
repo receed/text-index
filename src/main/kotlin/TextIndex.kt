6be7082478455087df812600c629a922d3d35a12
receed <efremov-343@yandex.ru>
2020-10-10 20:02:21 +0300

    Add comments to main file

--- src/main/kotlin/TextIndex.kt
+++ src/main/kotlin/TextIndex.kt
@@ -4,16 +4,26 @@ import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 import java.lang.IllegalStateException
 
+// Maximum number of non-empty lines on a page
 val LINES_PER_PAGE = 45
+
+// Parts of speech which should be ignored in index
 val ignoredParts = setOf("межд.", "союз", "част.", "предл.", "мс-п")
 
-
+// Exception for invalid input
 class InvalidInputException(message: String) : Exception(message)
 
+// Parses dictionary of forms from odict.ru
+// Uses odict.csv file in utf-8 charset
 class Dictionary {
+    // Part of sppech of the given word
     val partOfSpeech = mutableMapOf<String, String>()
+
+    // Words which should be ignored in index. Needed because one word may belong to several parts of speech
     val ignoredWords = mutableSetOf<String>("я", "он")
-    val defaultToForms =
+
+    // List of possible forms of given default form. Initialization fills partOfSpeech and ignoredWords
+    val defaultToForms: Map<String, List<String>> =
         File("odict.csv").readLines().map { line ->
             val words = line.toLowerCase().split(",")
             val defaultForm = words[0]
@@ -21,14 +31,16 @@ class Dictionary {
             if (words[1] in ignoredParts)
                 ignoredWords.add(defaultForm)
             defaultForm to words.drop(2) + defaultForm
-        }.filterNotNull().toMap().filterKeys { it !in ignoredWords }
+        }.filterNotNull().toMap().filterKeys { it !in ignoredWords } // removes function words
 
+    // List of forms corresponding to given default form
     val formToDefault =
         defaultToForms.map { (default, forms) ->
             forms.map { it to default }
         }.flatten().toMap()
 }
 
+// Reads file with text, returns list of non-blank lines
 fun readFile(fileName: String): List<String> {
     val file = File(fileName)
     if (!file.exists())
@@ -36,6 +48,7 @@ fun readFile(fileName: String): List<String> {
     return File(fileName).readLines().filter { it.isNotBlank() }
 }
 
+// Finds words consisting of Russian letters and hyphens in the given line
 fun lineToWords(line: String) =
     line.toLowerCase().replace(Regex("--|[^а-я-]"), " ").split(" ").filter { it.isNotBlank() }
 
@@ -52,11 +65,13 @@ fun getIndex(fileName: String): Index {
     }
 }
 
+// Writes lines to file or to result.txt
 fun writeFile(fileName: String?, lines: List<String>) {
     val a = lines[1][0]
     File(fileName ?: "result.txt").writeText(lines.joinToString("\n"))
 }
 
+// Wraps long lines. Returns list of lines not longer than [width]
 fun wrap(line: String, width: Int = 120): List<String> {
     val words = line.split(" ")
     val result = mutableListOf<StringBuilder>()
@@ -70,12 +85,14 @@ fun wrap(line: String, width: Int = 120): List<String> {
     return result.map { it.toString() }
 }
 
+// Entry point
 @ExperimentalCli
 fun main(args: Array<String>) {
     val parser = ArgParser("example")
     val input by parser.option(ArgType.String, shortName = "i", description = "Input file name").required()
     val output by parser.option(ArgType.String, shortName = "o", description = "Output file name")
 
+    // Subcommands for different modes
     class Index : Subcommand("index", "Create index of file") {
         override fun execute() {
             val lines = readFile(input)
